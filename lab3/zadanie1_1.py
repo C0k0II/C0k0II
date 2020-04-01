e5451da735d6ff25760ee4f7f405631d006fda84
Dmitry <89179508896d@gmail.com>
2020-04-01 22:56:31 +0400

    Make some ex. 1st day

---
+++ lab3/zadanie1_1.py
@@ -0,0 +1,24 @@
+from graph import *
+
+windowSize(1000,1000)
+
+penColor(0,0,0)
+penSize(1)
+brushColor("yellow")
+circle(300,300,100)
+
+penSize(20)
+rectangle(250,350,350,350)
+
+penSize(1)
+brushColor("red")
+circle(250,275,20)
+circle(350,275,15)
+brushColor("black")
+circle(250,275,10)
+circle(350,275,7)
+
+penSize(10)
+line(280,260,175,225)
+line(320,260,400,250)
+run()
